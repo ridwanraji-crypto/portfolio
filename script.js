ec9e55cc9460e9dd2ac6e65a1d704b02ba385091
ridwan <ridwanraji27@gmail.com>
2025-02-03 15:22:34 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+    const answer = item.querySelector('.faq-answer');
+
+    question.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+});
